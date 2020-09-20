8880b4add10e2350398e9e6b2ec8340d32546496
felipebogaertsm <felipebogaerts@gmail.com>
2020-09-20 17:26:56 -0300

    Version 1.0

--- Ballistics.py
+++ Ballistics.py
@@ -25,13 +25,13 @@ rain_length = 5
 # Time step (0.01 recommended) [s]:
 dt = 0.01
 # Drag coefficient:
-Cd = 0.4
+Cd = 0.5
 # Rocket mass (without motor) and payload mass [kg]:
-m_rocket, m_payload = 22, 5
+m_rocket, m_payload = 11, 4
 # Empty motor mass [kg]:
-m_motor = 17
+m_motor = 5
 # Rocket radius [m]:
-r = 0.5 * 160e-3
+r = 0.5 * 101.6e-3
 
 # Parachute data:
 # Time after apogee for drogue parachute activation [s]:
